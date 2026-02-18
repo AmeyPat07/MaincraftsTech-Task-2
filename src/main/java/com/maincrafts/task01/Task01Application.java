906554f1b0d5d6488014730096095ae6a3245d25
AmeyPat07 <ameypat07@gmail.com>
2026-02-18 15:25:53 +0530

    Initial commit

---
+++ src/main/java/com/maincrafts/task01/Task01Application.java
@@ -0,0 +1,11 @@
+package com.maincrafts.task01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task01Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Task01Application.class, args);
+    }
+}
